6598c41522f47efa614fc1e8fde0f4777148a954
Indian1999 <haszab99@gmail.com>
2025-12-12 23:19:18 +0100

    day5 done

--- day5/day5.py
+++ day5/day5.py
@@ -29,6 +29,44 @@ class FoodDatabase:
                 counter += 1
         return counter
     
+    def union_of_intervals(self, a, b):
+        if a[0] >= b[0] and a[1] <= b[1]:
+            return b
+        if b[0] >= a[0] and b[1] <= a[1]:
+            return a
+        if a[0] >= b[0] and a[0] <= b[1] and a[1] >= b[1]:
+            return (b[0], a[1])
+        if b[0] >= a[0] and b[0] <= a[1] and b[1] >= a[1]:
+            return (a[0], b[1])
+        return False
+        
+        
+
+    def count_number_of_fresh_ids(self):
+        i = 0
+        while i < len(self.ranges):
+            j = i + 1
+            while j < len(self.ranges):
+                union = self.union_of_intervals(self.ranges[i], self.ranges[j])
+                if union:
+                    self.ranges[i] = union
+                    del self.ranges[j]
+                    j = i + 1
+                else:
+                    j += 1
+            i += 1
+        counter = 0
+        for interval in self.ranges:
+            counter += interval[1] - interval[0] + 1
+        return counter
+
+    
 path = os.path.join(os.path.dirname(__file__), "day5_2.txt")
 fdb = FoodDatabase(path)
 print(fdb.count_fresh())
+print(fdb.count_number_of_fresh_ids())
+
+
+# Wrong answers (part 2):
+# 557297295790321
+# Correct: 339668510830757
